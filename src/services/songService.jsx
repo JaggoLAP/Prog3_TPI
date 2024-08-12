182e4c20a1a00a970c1621a86b8fce0f2e8e3072
JaggoLAP <107517813+JaggoLAP@users.noreply.github.com>
2024-08-11 22:10:19 -0300

    arreglada las urls

--- src/services/songService.jsx
+++ src/services/songService.jsx
@@ -11,13 +11,11 @@ const songService = {
       if (owner) {
         finalUrl.searchParams.append('owner', owner);
       }
-      if (url) {
-        console.log('url mandada: ', url)
-        if (url && url.startsWith('http://')) {
-          url = url.replace('http://', 'https://');
-        }
-        console.log('url mandada modificada: ', url)
+      
+      if (url && url.startsWith('http://')) {
+        finalUrl = finalUrl.replace('http://', 'https://');
       }
+
       if (!finalUrl.searchParams.has('page_size')) {
         finalUrl.searchParams.append('page_size', pageSize);
       }
