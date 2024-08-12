4760bba26c22329db99828e56ef1f9e901ce0064
JaggoLAP <107517813+JaggoLAP@users.noreply.github.com>
2024-08-11 23:09:16 -0300

    ultimo commit

--- src/components/NavIni.jsx
+++ src/components/NavIni.jsx
@@ -7,7 +7,7 @@ const NavIni = () => {
     
     <nav className="level">
     <figure className="image is-128x128 is-inline-block">
-                <img src="../public/Logo.png" alt="Sonirepro logo" />
+                <img src="/Logo.png" alt="Sonirepro logo" />
             </figure>
             <h1 className="title is-1 has-text-white">Sonirepro</h1>
       <div className="level-item has-text-centered">
